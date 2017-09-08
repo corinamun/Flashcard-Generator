2bf0f0773de80514bac6082b49a853c7a9e5424e
Corina Mun <corinamun@gmail.com>
2017-09-07 19:21:26 -0700

    final edits

--- BasicCard.js
+++ BasicCard.js
@@ -1,16 +1,6 @@
 function BasicCard (front,back) {
 	this.front = front;
 	this.back = back;
-	this.printStats = function () {
-		console.log(this.front);
-		console.log(this.back);
 };
 
-var firstPresident = new BasicCard(
-	"Who was the first president of the United States?", "George Washington");
-	console.log(firstPresident.back);
-
-}
-
-//end
-//module.exports = BasicCard
+module.exports = BasicCard;
